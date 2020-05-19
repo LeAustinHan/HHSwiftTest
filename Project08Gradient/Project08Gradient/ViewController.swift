c72382cd65e7992aaa0303b106909a07e02296c3
hanjize <314456527@qq.com>
2020-05-19 13:43:59 +0800

    fix description

--- Project08Gradient/Project08Gradient/ViewController.swift
+++ Project08Gradient/Project08Gradient/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
+    //指针属性和值属性都要有默认值
     var audioPlayer = AVAudioPlayer()
     
     let gradientLayer = CAGradientLayer()
     
-    var timer : Timer?
+    var timer : Timer?//默认值是nil
     
+    //实际backgroundColor 为计算类型属性。本身没有值，只是用来计算其他属性。
     var backgroundColor:(red: CGFloat, green: CGFloat,blue: CGFloat,alpha:CGFloat)!{
         didSet {
             let color1 = UIColor(red: backgroundColor.blue, green: backgroundColor.green, blue: 0, alpha: backgroundColor.alpha).cgColor
@@ -38,7 +39,7 @@ class ViewController: UIViewController {
         
         let bgMusic = URL(fileURLWithPath: Bundle.main.path(forResource: "Ecstasy", ofType: "mp3")!)
         
-        do {
+        do {//注意do catch写法
             try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
             try AVAudioSession.sharedInstance().setActive(true)
             try audioPlayer = AVAudioPlayer(contentsOf: bgMusic)
@@ -66,12 +67,12 @@ class ViewController: UIViewController {
         self.view.layer.addSublayer(gradientLayer)
     }
     
-    @objc func randomColor(){
+    @objc func randomColor(){//如果被selector调用，需要添加 @object修饰
         let redValue = CGFloat(drand48())
         let blueValue = CGFloat(drand48())
         let greenVlaue = CGFloat(drand48())
         
-        backgroundColor = (redValue,blueValue,greenVlaue,1)
+        backgroundColor = (redValue,blueValue,greenVlaue,1)//计算属性的调用
     }
 
 }
